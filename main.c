99889c022f4f5b6707614405ee3e4bddd85b8ac6
CrysFS <crystianschlemper@gmail.com>
2023-05-30 14:17:49 -0300

    feat: first commit

--- main.c
+++ main.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include "aluno.h"
 
+int main() {
+    int i = 0;
+    int opcao=0;
+    inicializar();
+    do{
+        printf("Menu principal\n");
+        printf("1. Cadastrar Aluno \n");
+        printf("2. Procurar Aluno \n");
+        printf("3. Lista todos \n");
+        printf("9. Sair \n");
+        printf("Digite sua opcao: \n");
+        scanf("%d", &opcao);
+        switch (opcao) {
+            case 1:
+                cadastrar(i);
+                i++;
+                break;
+            case 2:
+                procurar(i);
+                break;
+            case 3:
+                imprimirTudo(i);
+                break;
+            case 9:
+                printf("Fim do programa\n");
+                break;
+            default:
+                printf("Opcao invalida\n");
+        }
+    }while(opcao!=9);
+
+    return 0;
+}
